8169cacdcde7ed59291812daee7691d06d55a029
onurkaragunlu <onur_karagunlu@hotmail.com>
2019-10-12 20:28:40 +0300

    Pinned Shortcut was added

--- app/src/main/java/com/onurkaragunlu/appshortcutexample/MainActivity.kt
+++ app/src/main/java/com/onurkaragunlu/appshortcutexample/MainActivity.kt
@@ -8,11 +8,14 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
     var WRITE_ACTION = "com.onurkaragunlu.writing"
     var WRITE_EDIT = "com.onurkaragunlu.edit"
+    var DELETE = "com.onurkaragunlu.delete"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,8 +26,35 @@ class MainActivity : AppCompatActivity() {
             Log.d("SHORTCUTS", "SHORTCUT:WRITE")
         } else if (WRITE_EDIT.equals(intent.action)) {
             Log.d("SHORTCUTS", "SHORTCUT:EDIT")
+        } else if (DELETE.equals(intent.action)) {
+            Log.d("SHORTCUTS", "SHORTCUT:DELETE")
         }
         createShortCuts()
+
+        button.setOnClickListener {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val shortcutManager = getSystemService(ShortcutManager::class.java)
+               var pinnedShortcutList = shortcutManager.pinnedShortcuts
+                for (pinnedShortcut in pinnedShortcutList){
+                    if(pinnedShortcut.id.equals("delete")){
+                        Toast.makeText(applicationContext,"Daha Önce Eklendi.",Toast.LENGTH_LONG).show()
+                        return@setOnClickListener
+                    }
+                }
+
+                var shortcut = ShortcutInfo.Builder(this, "delete").apply {
+                    setShortLabel(applicationContext.getString(R.string.shortcut_shortname3))
+                    setLongLabel(applicationContext.getString(R.string.shortcut_longname3))
+                    setIcon(Icon.createWithResource(applicationContext, R.drawable.shortcut_delete))
+                    setIntent(Intent(applicationContext, MainActivity::class.java).apply {
+                        action = DELETE
+                    })
+                }.build()
+                shortcutManager.requestPinShortcut(shortcut, null);
+
+            }
+
+        }
     }
 
     fun createShortCuts() {
